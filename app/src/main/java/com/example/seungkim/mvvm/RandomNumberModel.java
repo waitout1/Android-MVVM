e87a134449cd595a0341fb7ccfd670aa0868eeb8
Heeya <waitout1@naver.com>
2017-05-27 11:10:55 +0900

    edit
    Model은 data다

--- app/src/main/java/com/example/seungkim/mvvm/RandomNumberModel.java
+++ app/src/main/java/com/example/seungkim/mvvm/RandomNumberModel.java
@@ -2,13 +2,10 @@ package com.example.seungkim.mvvm;
 
 
 public class RandomNumberModel {
-    private int number;
-
     public RandomNumberModel(){}
 
     public int getNumber() {
-        number = (int) (Math.random() * 99);
-        return number;
+        return (int) (Math.random() * 99);
     }
 }
 
